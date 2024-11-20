da0e295e6601128c14b9856b611108b447742d69
Alexey Borzov <avb@php.net>
2024-11-20 23:43:28 +0300

    Add a base ConnectionDecorator class

---
+++ src/sad_spirit/pg_wrapper/decorators/ConnectionDecorator.php
@@ -0,0 +1,227 @@
+<?php
+
+/**
+ * Converter of complex PostgreSQL types and an OO wrapper for PHP's pgsql extension
+ *
+ * LICENSE
+ *
+ * This source file is subject to BSD 2-Clause License that is bundled
+ * with this package in the file LICENSE and available at the URL
+ * https://raw.githubusercontent.com/sad-spirit/pg-wrapper/master/LICENSE
+ *
+ * @package   sad_spirit\pg_wrapper
+ * @copyright 2014-2024 Alexey Borzov
+ * @author    Alexey Borzov <avb@php.net>
+ * @license   https://opensource.org/licenses/BSD-2-Clause BSD 2-Clause license
+ * @link      https://github.com/sad-spirit/pg-wrapper
+ */
+
+declare(strict_types=1);
+
+namespace sad_spirit\pg_wrapper\decorators;
+
+use sad_spirit\pg_wrapper\{
+    Connection,
+    PreparedStatement,
+    Result,
+    TypeConverter,
+    TypeConverterFactory
+};
+use PgSql\Connection as NativeConnection;
+use Psr\Cache\CacheItemPoolInterface;
+
+/**
+ * Decorator for Connection class
+ *
+ * This base class delegates all method calls to the decorated instance, child classes will implement
+ * the additional logic
+ */
+abstract class ConnectionDecorator extends Connection
+{
+    /** @noinspection PhpMissingParentConstructorInspection */
+    public function __construct(private Connection $wrapped)
+    {
+    }
+
+    public function __destruct()
+    {
+        // No-op, logic from parent __destruct() is not needed here
+    }
+
+    public function __clone()
+    {
+        // No-op, logic from parent __clone() is not needed here
+    }
+
+    public function connect(): self
+    {
+        $this->wrapped->connect();
+
+        return $this;
+    }
+
+
+    public function disconnect(): self
+    {
+        $this->wrapped->disconnect();
+
+        return $this;
+    }
+
+    public function isConnected(): bool
+    {
+        return $this->wrapped->isConnected();
+    }
+
+    public function getLastError(): ?string
+    {
+        return $this->wrapped->getLastError();
+    }
+
+    public function getNative(): NativeConnection
+    {
+        return $this->wrapped->getNative();
+    }
+
+    public function getConnectionId(): string
+    {
+        return $this->wrapped->getConnectionId();
+    }
+
+    public function quote(mixed $value, mixed $type = null): string
+    {
+        return $this->wrapped->quote($value, $type);
+    }
+
+    public function quoteIdentifier(string $identifier): string
+    {
+        return $this->wrapped->quoteIdentifier($identifier);
+    }
+
+    public function prepare(string $query, array $paramTypes = [], array $resultTypes = []): PreparedStatement
+    {
+        return $this->wrapped->prepare($query, $paramTypes, $resultTypes);
+    }
+
+    public function execute(string $sql, array $resultTypes = []): Result
+    {
+        return $this->wrapped->execute($sql, $resultTypes);
+    }
+
+    public function executeParams(string $sql, array $params, array $paramTypes = [], array $resultTypes = []): Result
+    {
+        return $this->wrapped->executeParams($sql, $params, $paramTypes, $resultTypes);
+    }
+
+    public function getTypeConverterFactory(): TypeConverterFactory
+    {
+        return $this->wrapped->getTypeConverterFactory();
+    }
+
+    public function setTypeConverterFactory(TypeConverterFactory $factory): self
+    {
+        $this->wrapped->setTypeConverterFactory($factory);
+
+        return $this;
+    }
+
+    public function getTypeConverter(mixed $type): TypeConverter
+    {
+        return $this->wrapped->getTypeConverter($type);
+    }
+
+    public function getMetadataCache(): ?CacheItemPoolInterface
+    {
+        return $this->wrapped->getMetadataCache();
+    }
+
+    public function setMetadataCache(CacheItemPoolInterface $cache): self
+    {
+        $this->wrapped->setMetadataCache($cache);
+
+        return $this;
+    }
+
+    public function beginTransaction(): self
+    {
+        $this->wrapped->beginTransaction();
+
+        return $this;
+    }
+
+    public function commit(): self
+    {
+        $this->wrapped->commit();
+
+        return $this;
+    }
+
+    public function rollback(): self
+    {
+        $this->wrapped->rollback();
+
+        return $this;
+    }
+
+    public function createSavepoint(string $savepoint): self
+    {
+        $this->wrapped->createSavepoint($savepoint);
+
+        return $this;
+    }
+
+    public function releaseSavepoint(string $savepoint): self
+    {
+        $this->wrapped->releaseSavepoint($savepoint);
+
+        return $this;
+    }
+
+    public function rollbackToSavepoint(string $savepoint): self
+    {
+        $this->wrapped->rollbackToSavepoint($savepoint);
+
+        return $this;
+    }
+
+    public function inTransaction(): bool
+    {
+        return $this->wrapped->inTransaction();
+    }
+
+    public function atomic(callable $callback, bool $savepoint = false): mixed
+    {
+        return $this->wrapped->atomic(fn() => $callback($this), $savepoint);
+    }
+
+    public function onCommit(callable $callback): self
+    {
+        $this->wrapped->onCommit($callback);
+
+        return $this;
+    }
+
+    public function onRollback(callable $callback): self
+    {
+        $this->wrapped->onRollback($callback);
+
+        return $this;
+    }
+
+    public function needsRollback(): bool
+    {
+        return $this->wrapped->needsRollback();
+    }
+
+    public function setNeedsRollback(bool $needsRollback): self
+    {
+        $this->wrapped->setNeedsRollback($needsRollback);
+
+        return $this;
+    }
+
+    public function assertRollbackNotNeeded(): void
+    {
+        $this->wrapped->assertRollbackNotNeeded();
+    }
+}
---
+++ tests/decorators/ConnectionDecoratorTest.php
@@ -0,0 +1,258 @@
+<?php
+
+/**
+ * Converter of complex PostgreSQL types and an OO wrapper for PHP's pgsql extension
+ *
+ * LICENSE
+ *
+ * This source file is subject to BSD 2-Clause License that is bundled
+ * with this package in the file LICENSE and available at the URL
+ * https://raw.githubusercontent.com/sad-spirit/pg-wrapper/master/LICENSE
+ *
+ * @package   sad_spirit\pg_wrapper
+ * @copyright 2014-2024 Alexey Borzov
+ * @author    Alexey Borzov <avb@php.net>
+ * @license   https://opensource.org/licenses/BSD-2-Clause BSD 2-Clause license
+ * @link      https://github.com/sad-spirit/pg-wrapper
+ */
+
+declare(strict_types=1);
+
+namespace sad_spirit\pg_wrapper\tests\decorators;
+
+use sad_spirit\pg_wrapper\{
+    Connection,
+    decorators\ConnectionDecorator,
+    PreparedStatement,
+    Result,
+    TypeConverter,
+    TypeConverterFactory
+};
+use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheItemPoolInterface;
+
+/**
+ * Unit test for ConnectionDecorator class
+ */
+class ConnectionDecoratorTest extends TestCase
+{
+    public function testConnecting(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('connect');
+        $connection->expects($this->once())
+            ->method('isConnected')
+            ->willReturn(true);
+        $connection->expects($this->once())
+            ->method('disconnect');
+
+        $this::assertSame($decorator, $decorator->connect());
+        $this::assertTrue($decorator->isConnected());
+        $this::assertSame($decorator, $decorator->disconnect());
+    }
+
+    public function testGetLastError(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $connection->expects($this->once())
+            ->method('getLastError')
+            ->willReturn('oh noes!');
+
+        $this::assertEquals('oh noes!', $this->createDecorator($connection)->getLastError());
+    }
+
+    public function testGetNative(): void
+    {
+        // We cannot mock \PgSql\Connection, so just throw an exception
+        $connection = $this->createMock(Connection::class);
+        $connection->expects($this->once())
+            ->method('getNative')
+            ->willThrowException(new \Exception('oh noes!'));
+
+        $this::expectException(\Exception::class);
+        $this::expectExceptionMessage('oh noes!');
+        $this->createDecorator($connection)->getNative();
+    }
+
+    public function testGetConnectionId(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $connection->expects($this->once())
+            ->method('getConnectionId')
+            ->willReturn('an id');
+
+        $this::assertEquals('an id', $this->createDecorator($connection)->getConnectionId());
+    }
+
+    public function testQuoting(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('quote')
+            ->willReturn('quoted value');
+        $connection->expects($this->once())
+            ->method('quoteIdentifier')
+            ->willReturn('quoted identifier');
+
+        $this::assertEquals('quoted value', $decorator->quote('some value'));
+        $this::assertEquals('quoted identifier', $decorator->quoteIdentifier('some identifier'));
+    }
+
+    public function testPrepare(): void
+    {
+        $statement  = $this->createMock(PreparedStatement::class);
+        $connection = $this->createMock(Connection::class);
+        $connection->expects($this->once())
+            ->method('prepare')
+            ->willReturn($statement);
+
+        $this::assertSame($statement, $this->createDecorator($connection)->prepare('select 1'));
+    }
+
+    public function testExecute(): void
+    {
+        $result     = $this->createMock(Result::class);
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('execute')
+            ->with('select 1')
+            ->willReturn($result);
+        $connection->expects($this->once())
+            ->method('executeParams')
+            ->with('select $1', [1])
+            ->willReturn($result);
+
+        $this::assertSame($result, $decorator->execute('select 1'));
+        $this::assertSame($result, $decorator->executeParams('select $1', [1]));
+    }
+
+    public function testConverters(): void
+    {
+        $converter  = $this->createMock(TypeConverter::class);
+        $factory    = $this->createMock(TypeConverterFactory::class);
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('setTypeConverterFactory')
+            ->with($factory);
+        $connection->expects($this->once())
+            ->method('getTypeConverterFactory')
+            ->willReturn($factory);
+        $connection->expects($this->once())
+            ->method('getTypeConverter')
+            ->with('foo')
+            ->willReturn($converter);
+
+        $this::assertSame($decorator, $decorator->setTypeConverterFactory($factory));
+        $this::assertSame($factory, $decorator->getTypeConverterFactory());
+        $this::assertSame($converter, $decorator->getTypeConverter('foo'));
+    }
+
+    public function testMetadataCache(): void
+    {
+        $cache = $this->createMock(CacheItemPoolInterface::class);
+        $connection = $this->createMock(Connection::class);
+        $connection->expects($this->once())
+            ->method('setMetadataCache')
+            ->with($cache);
+        $connection->expects($this->once())
+            ->method('getMetadataCache')
+            ->willReturn($cache);
+        $decorator  = $this->createDecorator($connection);
+
+        $this::assertSame($decorator, $decorator->setMetadataCache($cache));
+        $this::assertSame($cache, $decorator->getMetadataCache());
+    }
+
+    public function testTransactions(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('beginTransaction');
+        $connection->expects($this->once())
+            ->method('commit');
+        $connection->expects($this->once())
+            ->method('rollback');
+        $connection->expects($this->once())
+            ->method('inTransaction')
+            ->willReturn(true);
+
+        $this::assertSame($decorator, $decorator->beginTransaction());
+        $this::assertSame($decorator, $decorator->commit());
+        $this::assertSame($decorator, $decorator->rollback());
+        $this::assertTrue($decorator->inTransaction());
+    }
+
+    public function testSavepoints(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('createSavepoint');
+        $connection->expects($this->once())
+            ->method('releaseSavepoint');
+        $connection->expects($this->once())
+            ->method('rollbackToSavepoint');
+
+        $this::assertSame($decorator, $decorator->createSavepoint('foo'));
+        $this::assertSame($decorator, $decorator->releaseSavepoint('foo'));
+        $this::assertSame($decorator, $decorator->rollbackToSavepoint('foo'));
+    }
+
+    public function testAtomic(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('atomic')
+            ->willReturnArgument(0);
+
+        $callback = $decorator->atomic(fn(Connection $c) => $this::assertSame($decorator, $c));
+        $callback();
+    }
+
+    public function testTransactionCallbacks(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('onCommit');
+        $connection->expects($this->once())
+            ->method('onRollback');
+
+        $this::assertSame($decorator, $decorator->onCommit(fn() => true));
+        $this::assertSame($decorator, $decorator->onRollback(fn() => false));
+    }
+
+    public function testRollbackNeeded(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $decorator  = $this->createDecorator($connection);
+        $connection->expects($this->once())
+            ->method('setNeedsRollback')
+            ->with(true);
+        $connection->expects($this->once())
+            ->method('needsRollback')
+            ->willReturn(false);
+        $connection->expects($this->once())
+            ->method('assertRollbackNotNeeded')
+            ->willThrowException(new \Exception('oh noes!'));
+
+        $this::assertSame($decorator, $decorator->setNeedsRollback(true));
+        $this::assertFalse($decorator->needsRollback());
+        $this::expectException(\Exception::class);
+        $this::expectExceptionMessage('oh noes!');
+        $decorator->assertRollbackNotNeeded();
+    }
+
+    private function createDecorator(Connection $wrapped): ConnectionDecorator
+    {
+        return new class ($wrapped) extends ConnectionDecorator {
+        };
+    }
+}
